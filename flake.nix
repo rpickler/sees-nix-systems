7a349da15059a309a7e154524372ef8409111600
Richard Pickler <rpickler@gmail.com>
2025-09-09 20:27:43 +0100

    Properly addressed sees-nix-packages on github

--- flake.nix
+++ flake.nix
@@ -1,11 +1,29 @@
 {
   description = "Sees.Ai Nixos Configurations";
 
+  nixConfig = {
+    access-tokens = [
+      "github.com=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"
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    sops-nix.url = "github:mic92/sops-nix";
-    disko.url = "github:nvmd/disko/gpt-attrs";
-    sees-ai.url = "/home/rpickler/devel/sees.ai/sees-nix-packages";
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nvmd/disko/gpt-attrs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sees-ai = {
+      url = "github:rpickler/sees-nix-packages";
+      #url = "/home/rpickler/devel/sees.ai/sees-nix-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: 
