ee9c0360f57d40c1f3790cd39ad4ef45a5c5f27a
Richard Pickler <rpickler@gmail.com>
2025-09-09 10:56:06 +0100

    Added hardware for nuc and removed encrypted disk

--- systems/nuc/filesystems.nix
+++ systems/nuc/filesystems.nix
@@ -29,43 +29,32 @@
                 ];
               };
             };
-            luks = {
+            system = {
               size = "100%";
-              label = "luks";
+              label = "system";
               content = {
-                type = "luks";
-                name = "cryptroot";
-                extraOpenArgs = [
-                  "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
-                # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
-                settings = {crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10"];};
-                content = {
-                  type = "btrfs";
-                  extraArgs = ["-L" "nixos" "-f"];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = ["subvol=root" "compress=zstd" "noatime"];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = ["subvol=home" "compress=zstd" "noatime"];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
-                    };
-                    "/log" = {
-                      mountpoint = "/var/log";
-                      mountOptions = ["subvol=log" "compress=zstd" "noatime"];
-                    };
-                    "/swap" = {
-                      mountpoint = "/swap";
-                      swap.swapfile.size = "96G";
-                    };
+                type = "btrfs";
+                extraArgs = ["-L" "nixos" "-f"];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                  };
+                  "/log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                  };
+                  "/swap" = {
+                    mountpoint = "/swap";
+                    swap.swapfile.size = "96G";
                   };
                 };
               };
--- systems/nuc/system.nix
+++ systems/nuc/system.nix
@@ -16,8 +16,13 @@ let
       isNormalUser = true;
       description = "Richard Pickler";
       extraGroups = [ "networkmanager" "wheel" "video" ];
+      hashedPassword = "$y$j9T$c9HsBtDvOugGTPwXwMFwY1$Ey/PmzgJaa3E/CloTfQZ5BoGAak3Xq7MdmtRVAsK7./";
     };
     security.sudo.wheelNeedsPassword = false;
+
+    nix.settings.trusted-users = [ "rpickler" ];
+
+    services.openssh.enable = true;
   };
 in
 { 
@@ -26,6 +31,7 @@ in
     {nixpkgs.overlays = [ inputs.sees-ai.overlay ];}
     inputs.sops-nix.nixosModules.sops
     inputs.disko.nixosModules.disko
+    ./hardware.nix
     ./filesystems.nix
     ./SeesInterface2.nix
     config
